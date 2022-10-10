71b7f2e723a3e9266effea0b4d080d48f3cf7ca0
Aritra-chatterje <aritrachatterjee207@gmail.com>
2022-10-10 22:56:01 +0530

    modified code

--- problem4.js
+++ problem4.js
@@ -1,4 +1,9 @@
-let a=5;
-let b=6;
-let c=8;
-(a>b && a>c)? console.log("A"):(b>a && b>c)? console.log("B"):console.log("C")
+let a = 5;
+let b = 6;
+let c = 8;
+a > b && a > c
+  ? console.log("A")
+  : b > a && b > c
+  ? console.log("B")
+  : console.log("C");
+const n=20
