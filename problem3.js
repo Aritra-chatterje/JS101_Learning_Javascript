e0efef273fc46013588a071a0be2409739c4cc5b
Aritra-chatterje <aritrachatterjee207@gmail.com>
2022-10-10 22:22:09 +0530

    this is full project

---
+++ problem3.js
@@ -0,0 +1,6 @@
+let a ="j"
+if(a=="a" || a=="e" || a=="i" || a=="o" || a=="u"){
+  console.log("vowel")
+}else{
+  console.log("consonent")
+}
---
+++ problem4.js
@@ -0,0 +1,4 @@
+let a=5;
+let b=6;
+let c=8;
+(a>b && a>c)? console.log("A"):(b>a && b>c)? console.log("B"):console.log("C")
